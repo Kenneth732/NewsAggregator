e6a9e0707e3d43141f66cd87644846152f945f64
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-30 10:49:54 +0300

    adding apis_k

--- src/components/NewsAggregator.js
+++ src/components/NewsAggregator.js
@@ -2,3 +2,5 @@ import React, { useState, useEffect } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import './NewsAggregator.css';
+
+const API_KEY = '6cbdef411b3b47529a17b5cf4667303b'; // Replace with your NewsAPI key

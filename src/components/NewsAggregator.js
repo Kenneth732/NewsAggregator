c513b3ebffc58473d242da4701b0bf100d0bd5e9
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-30 10:49:07 +0300

    import react state useEffect and import react-lazyload image

---
+++ src/components/NewsAggregator.js
@@ -0,0 +1,4 @@
+import React, { useState, useEffect } from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import './NewsAggregator.css';

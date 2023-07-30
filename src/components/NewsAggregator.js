8c8c0599a01506351d18d4cc074f0d12c57b84f7
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-30 10:51:02 +0300

    declare new Varibale function with useEffec for fetchinf data

--- src/components/NewsAggregator.js
+++ src/components/NewsAggregator.js
@@ -4,3 +4,20 @@ import 'react-lazy-load-image-component/src/effects/blur.css';
 import './NewsAggregator.css';
 
 const API_KEY = '6cbdef411b3b47529a17b5cf4667303b'; // Replace with your NewsAPI key
+
+const NewsAggregator = () => {
+  const [newsHeadlines, setNewsHeadlines] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    if (searchTerm.trim() === '') {
+      fetchTopHeadlines();
+    } else {
+      fetchNewsHeadlinesWithSearch();
+    }
+  }, [searchTerm]);
+
+
+};
+
+export default NewsAggregator;

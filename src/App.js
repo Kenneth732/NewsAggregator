8164ba2c452b1b1d94c4c5c4622ed8c87f125f4d
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-30 10:47:20 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NewsAggregator from './components/NewsAggregator';
+
+function App() {
+  return (
+    <div className="App">
+      <NewsAggregator />
+    </div>
+  );
+}
+
+export default App;

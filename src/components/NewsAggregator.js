eb8d0c3e0481961e8e731b1aaabd3d45f394b3de
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-09-08 13:55:24 +0300

    news

--- src/components/NewsAggregator.js
+++ src/components/NewsAggregator.js
@@ -3,12 +3,12 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import './NewsAggregator.css';
 
-
 const API_KEY = '6cbdef411b3b47529a17b5cf4667303b'; // Replace with your NewsAPI key
 
 const NewsAggregator = () => {
   const [newsHeadlines, setNewsHeadlines] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [selectedNews, setSelectedNews] = useState(null); // State for the selected news article
 
   useEffect(() => {
     if (searchTerm.trim() === '') {
@@ -44,35 +44,45 @@ const NewsAggregator = () => {
     setSearchTerm(e.target.value);
   };
 
+  // Function to handle the "Read More" click and set the selected news article
+  const handleReadMoreClick = (newsItem) => {
+    setSelectedNews(newsItem);
+  };
+
   return (
     <div className="news-aggregator">
       <h2>News Aggregator</h2>
       <div className='news'>
         <div className="news-list">
-
           {newsHeadlines.map((news, index) => (
-            // Check if it's not the first card (index > 0) before rendering
-            index > 0 && (
-              <div className="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20" key={index}>
-                <div className="flex justify-center md:justify-end -mt-16">
-                  <img className="w-20 h-20 object-cover rounded-full border-2 border-indigo-500" src={news.urlToImage || 'placeholder.jpg'} alt="News" />
-                </div>
-                <div>
-                  <h2 className="text-gray-800 text-3xl font-semibold">{news.title}</h2>
-                  <p className="mt-2 text-gray-600">{news.author} && By {news.author}</p>
-                  <p className="mt-2 text-gray-600">{news.description}</p>
-                </div>
-                <div className="flex justify-end mt-4">
-                  <a href={news} target="_blank" rel="noopener noreferrer" className="text-xl font-medium text-indigo-500">
-                    Read More
-                  </a>
-                </div>
+            <div className="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20" key={index}>
+              <div className="flex justify-center md:justify-end -mt-16">
+                <img className="w-20 h-20 object-cover rounded-full border-2 border-indigo-500" src={news.urlToImage || 'placeholder.jpg'} alt="News" />
               </div>
-            )
+              <div>
+                <h2 className="text-gray-800 text-3xl font-semibold">{news.title}</h2>
+                <p className="mt-2 text-gray-600">{news.author} && By {news.author}</p>
+                <p className="mt-2 text-gray-600">{news.description}</p>
+              </div>
+              <div className="flex justify-end mt-4">
+                <a href={news.url} target="_blank" rel="noopener noreferrer" className="text-xl font-medium text-indigo-500">
+                  {/* Pass the current news item to the click handler */}
+                  <button onClick={() => handleReadMoreClick(news)}>Read More</button>
+                </a>
+              </div>
+            </div>
           ))}
         </div>
 
         <div className='middle'>
+          {selectedNews && (
+            <div className="selected-news">
+              <h2>{selectedNews.title}</h2>
+              <p>{selectedNews.description}</p>
+              <a href={selectedNews.url} target="_blank" rel="noopener noreferrer">Read Full Article</a>
+            </div>
+          )}
+
           <div className="search-form">
             <input
               type="text"
@@ -84,8 +94,8 @@ const NewsAggregator = () => {
           </div>
         </div>
       </div>
-      </div>
-      );
+    </div>
+  );
 };
 
-      export default NewsAggregator;
+export default NewsAggregator;
